b268a696885f5f0b6ef7fcfb1f84f6f379dc6840
AmbBAI <amb.main@gmail.com>
2015-05-27 19:38:31 +0800

    line

--- spatial_subdivision/helloworld.cpp
+++ spatial_subdivision/helloworld.cpp
@@ -5,7 +5,29 @@
 
 #include "common.h"
 #include "bgfx_utils.h"
+#include <vector>
 using namespace std;
+
+struct PosColorVertex
+{
+	float m_x;
+	float m_y;
+	float m_z;
+	uint32_t m_abgr;
+
+	static void init()
+	{
+		ms_decl
+			.begin()
+			.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+			.add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true)
+			.end();
+	};
+
+	static bgfx::VertexDecl ms_decl;
+};
+
+bgfx::VertexDecl PosColorVertex::ms_decl;
 
 int _main_(int /*_argc*/, char** /*_argv*/)
 {
@@ -30,6 +52,52 @@ int _main_(int /*_argc*/, char** /*_argv*/)
 
 	Mesh* mesh = meshLoad("meshes/bunny.bin");
 	bgfx::ProgramHandle program = loadProgram("vs_default", "fs_default");
+
+	Aabb aabb = mesh->m_groups[0].m_aabb;
+	for (auto g : mesh->m_groups)
+	{
+		aabb.m_min[0] = bx::fmin(g.m_aabb.m_min[0], aabb.m_min[0]);
+		aabb.m_min[1] = bx::fmin(g.m_aabb.m_min[1], aabb.m_min[1]);
+		aabb.m_min[2] = bx::fmin(g.m_aabb.m_min[2], aabb.m_min[2]);
+		aabb.m_max[0] = bx::fmin(g.m_aabb.m_max[0], aabb.m_max[0]);
+		aabb.m_max[1] = bx::fmin(g.m_aabb.m_max[1], aabb.m_max[1]);
+		aabb.m_max[2] = bx::fmin(g.m_aabb.m_max[2], aabb.m_max[2]);
+	}
+	 
+	PosColorVertex meshVertices[8] = {
+		{ aabb.m_min[0], aabb.m_min[1], aabb.m_min[2], 0xffffff },
+		{ aabb.m_min[0], aabb.m_min[1], aabb.m_max[2], 0xffffff },
+		{ aabb.m_min[0], aabb.m_max[1], aabb.m_min[2], 0xffffff },
+		{ aabb.m_min[0], aabb.m_max[1], aabb.m_max[2], 0xffffff },
+		{ aabb.m_max[0], aabb.m_min[1], aabb.m_min[2], 0xffffff },
+		{ aabb.m_max[0], aabb.m_min[1], aabb.m_max[2], 0xffffff },
+		{ aabb.m_max[0], aabb.m_max[1], aabb.m_min[2], 0xffffff },
+		{ aabb.m_max[0], aabb.m_max[1], aabb.m_max[2], 0xffffff }
+	};
+
+	uint32_t meshIndices[24] = {
+		0, 1, /**/ 0, 2, /**/ 1, 3, /**/ 2, 3,
+		4, 5, /**/ 4, 6, /**/ 5, 7, /**/ 6, 7,
+		0, 4, /**/ 1, 5, /**/ 2, 6, /**/ 3, 7
+	};
+
+	// Create vertex stream declaration.
+	PosColorVertex::init();
+
+	// Create static vertex buffer.
+	bgfx::VertexBufferHandle vbh = bgfx::createVertexBuffer(
+		// Static data can be passed with bgfx::makeRef
+		bgfx::makeRef(meshVertices, sizeof(meshVertices))
+		, PosColorVertex::ms_decl
+		);
+
+	// Create static index buffer.
+	bgfx::IndexBufferHandle ibh = bgfx::createIndexBuffer(
+		// Static data can be passed with bgfx::makeRef
+		bgfx::makeRef(meshIndices, sizeof(meshIndices))
+		);
+
+	bgfx::ProgramHandle lineProgram = loadProgram("vs_line", "fs_line");
 
 	while (!entry::processEvents(width, height, debug, reset))
 	{
@@ -89,6 +157,12 @@ int _main_(int /*_argc*/, char** /*_argv*/)
 		float mtx[16];
 		bx::mtxRotateY(mtx, 90.f);
 		meshSubmit(mesh, 0, program, mtx);
+
+		bgfx::setVertexBuffer(vbh);
+		bgfx::setIndexBuffer(ibh);
+		bgfx::setProgram(lineProgram);
+		bgfx::setState(BGFX_STATE_PT_LINES);
+		bgfx::submit(0);
 
 		// Advance to next frame. Rendering thread will be kicked to 
 		// process submitted rendering primitives.
--- spatial_subdivision/shaderc.bat
+++ spatial_subdivision/shaderc.bat
@@ -5,3 +5,5 @@ set outDir="../runtime/shaders/glsl/"
 %shadercExe% -f vs_default.sc -i %includePath% -o %outDir%vs_default.bin --platform linux --type v --varyingdef varying.def.sc
 %shadercExe% -f fs_default.sc -i %includePath% -o %outDir%fs_default.bin --platform linux --type f --varyingdef varying.def.sc
 
+%shadercExe% -f vs_line.sc -i %includePath% -o %outDir%vs_line.bin --platform linux --type v --varyingdef varying.def.sc
+%shadercExe% -f fs_line.sc -i %includePath% -o %outDir%fs_line.bin --platform linux --type f --varyingdef varying.def.sc
